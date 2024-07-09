b6baad7cf7289446ead6c0668677f349ac49f37c
Mark Swaringen <mswaringen@gmail.com>
2024-07-09 12:37:47 +0700

    init

---
+++ Pelican/content/pages/about.md
@@ -0,0 +1,13 @@
+Title: About
+
+A place to save content for my future reference
+
+[Github](https://github.com/mswaringen/)
+
+----
+
+### built with
+- [Pelican](https://getpelican.com/)
+- [Simple theme](https://www.paulox.net/2023/11/30/pelican-4.9-classless-simple-theme/)
+- [Water CSS](https://watercss.kognise.dev/)
+- [Github Pages](https://pages.github.com/)
---
+++ Pelican/pelicanconf.py
@@ -0,0 +1,42 @@
+AUTHOR = 'anon'
+SITENAME = 'AiAnonymousPT'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Lisbon'
+AUTHOR_SAVE_AS = ''
+
+DEFAULT_LANG = 'en'
+
+THEME = "simple"
+# STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+# STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.6/css/pico.classless.lime.min.css"
+STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.6/css/pico.classless.pumpkin.min.css"
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
